20d00fa0776432dcee7a04ee6f09b8f47b59f614
Fabian <Fabian@DESKTOP-OHOU5E1>
2018-07-05 11:58:45 +0200

    Projektdateien hinzufügen.

---
+++ MVC4_CSharp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4_CSharp.Controllers
+{
+    public struct Customer
+    {
+        public string Name;
+        public int Alter;
+        public string EMail;
+        public string Password;
+    }
+
+    public class HomeController : Controller
+    {
+        public static int Counter = 1;
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData.Add(new KeyValuePair<string, object>("greetings", "Hallo from Asp.Net :)"));
+            ViewData.Add(new KeyValuePair<string, object>("counter", Counter++));
+
+            Customer Fabian = new Customer();
+            Fabian.Alter = 32;
+            Fabian.EMail = "nraseru@googlemail.com";
+            Fabian.Name = "Fabian";
+            Fabian.Password = "geheim";
+
+            ViewBag.data = Fabian;
+
+            return View();
+        }
+
+        public ActionResult Seite2()
+        {
+            ViewBag.Title = "Hello world";
+            return View();
+        }
+
+        public ActionResult Seite3()
+        {
+            return View();
+        }
+
+        public ActionResult Testform()
+        {
+
+            return View();
+        }
+    }
+}
